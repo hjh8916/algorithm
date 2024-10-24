fdee7fa869001109fa2f629a42f074df31a9b5b9
jonny.h.rhyme <jonny.h.rhyme@gmail.com>
2024-10-24 23:00:33 +0900

    add module

---
+++ module/app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+// TODO
+// 1. 입력값 설정
+// 2. solution parm 설정
+
+const input = fs.readFileSync(`${__dirname}/data.txt`).toString();
+
+
+const solution = () => {
+   
+}
+
+const result = solution(n, arr);
+console.log(result);
+
+
+
